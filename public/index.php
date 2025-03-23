17d485bad492814588a1ffc40ffee46a5da7eefc
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-03-23 04:18:59 -0300

    configure el public/index y cambie el app por src

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 
 
-require_once __DIR__ . '/../autoload.php';
-//require_once __DIR__ . '/../vendor/autoload.php';
+//require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';  // Ruta correcta desde public/index.php
+
 
 
 require_once __DIR__ . '/../routes/web.php';

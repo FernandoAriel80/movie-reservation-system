db607616eddf07878fc4f111dcd568c98518a2b7
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-03-23 04:07:20 -0300

    configure el public/index

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 
 
-require_once __DIR__ . '/../autoload.php';
+//require_once __DIR__ . '/../autoload.php';v
+require_once __DIR__ . '/../vendor/autoload.php';
 
 
 require_once __DIR__ . '/../routes/web.php';
